e9f210ec6da234682800f6fdc5987443be66abec
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-02-14 17:18:18 +0900

    replace to %i

--- crkbd.rb
+++ crkbd.rb
@@ -1,56 +1,37 @@
-require "sounder"
-# Initialize a Keyboard
 kbd = Keyboard.new
-
-# `split=` should happen before `init_pins`
 kbd.split = true
-
 kbd.mutual_uart_at_my_own_risk = true
-
-# If your right hand of CRKBD is the "anchor"
-# kbd.set_anchor(:right)
-
-# Initialize GPIO assign
 kbd.init_pins(
   [ 4, 5, 6, 7 ],            # row0, row1,... respectively
   [ 29, 28, 27, 26, 18, 20 ] # col0, col1,... respectively
 )
 
-kbd.add_layer :default, [
-# LEFT BOARD                                                   #RIGHT BOARD
-#|           |       |       |           |           |         |         |           |          |        |           |
-  :KC_ESCAPE, :KC_Q,  :KC_W,  :KC_E,      :KC_R,      :KC_T,    :KC_Y,    :KC_U,      :KC_I,     :KC_O,   :KC_P,      :KC_BSLASH,
-  :KC_TAB,    :KC_A,  :KC_S,  :KC_D,      :KC_F,      :KC_G,    :KC_H,    :KC_J,      :KC_K,     :KC_L,   :KC_SCOLON, :KC_QUOTE,
-  :KC_LSFT,   :KC_Z,  :KC_X,  :KC_C,      :KC_V,      :KC_B,    :KC_N,    :KC_M,      :KC_COMMA, :KC_DOT, :KC_SLASH,  :KC_MINUS,
-  :KC_NO,     :KC_NO, :KC_NO, :KC_BSPACE, :LOWER_SPC, :KC_LCTL, :KC_RGUI, :RAISE_ENT, :KC_RALT,  :KC_NO,  :KC_NO,     :KC_NO,
-#|           |       |       |           |           |         |         |           |          |        |           |
-]
-kbd.add_layer :raise, [
-# LEFT BOARD                                                       #RIGHT BOARD
-#|          |         |         |             |         |          |         |           |          |          |          |
-  :KC_GRAVE, :KC_EXLM, :KC_AT,   :KC_HASH,     :KC_DLR,  :KC_PERC,  :KC_CIRC, :KC_AMPR,   :KC_ASTER, :KC_LPRN,  :KC_RPRN,  :KC_PIPE,
-  :KC_TAB,   :KC_LABK, :KC_LCBR, :KC_LBRACKET, :KC_LPRN, :KC_QUOTE, :KC_LEFT, :KC_DOWN,   :KC_UP,    :KC_RIGHT, :KC_UNDS,  :KC_PLUS,
-  :KC_LSFT,  :KC_RABK, :KC_RCBR, :KC_RBRACKET, :KC_RPRN, :KC_DQUO,  :KC_TILD, :KC_BSLASH, :KC_COMMA, :KC_DOT,   :KC_SLASH, :KC_EQUAL,
-  :KC_NO,    :KC_NO,   :KC_NO,   :KC_LANG2,    :ADJUST,  :KC_LCTL,  :KC_RGUI, :RAISE_ENT, :KC_RALT,  :KC_NO,    :KC_NO,    :KC_NO,
-#|          |         |         |             |         |          |         |           |          |          |          |
-]
-kbd.add_layer :lower, [
-# LEFT BOARD                                                #RIGHT BOARD
-#|           |         |         |             |            |          |        |        |          |       |          |
-  :KC_ESCAPE, :KC_1,    :KC_2,    :KC_3,        :KC_4,       :KC_5,     :KC_6,   :KC_7,   :KC_8,     :KC_9,  :KC_0,     :KC_PLUS,
-  :KC_TAB,    :KC_NO,   :KC_NO,   :KC_NO,       :KC_LPRN,    :KC_QUOTE, :KC_DOT, :KC_4,   :KC_5,     :KC_6,  :KC_PLUS,  :KC_ASTER,
-  :KC_LSFT,   :KC_RABK, :KC_RCBR, :KC_RBRACKET, :KC_RPRN,    :KC_DQUO,  :KC_0,   :KC_1,   :KC_2,     :KC_3,  :KC_SLASH, :KC_COMMA,
-  :KC_NO,     :KC_NO,   :KC_NO,   :KC_BSPACE,    :LOWER_SPC, :KC_LCTL,  :KC_RGUI,:ADJUST, :KC_LANG1, :KC_NO, :KC_NO,    :KC_NO,
-#|           |         |         |             |            |          |        |        |          |       |          |
+kbd.add_layer :default, %i[
+  KC_ESCAPE KC_Q  KC_W  KC_E      KC_R      KC_T    KC_Y    KC_U      KC_I     KC_O   KC_P      KC_BSLASH
+  KC_TAB    KC_A  KC_S  KC_D      KC_F      KC_G    KC_H    KC_J      KC_K     KC_L   KC_SCOLON KC_QUOTE
+  KC_LSFT   KC_Z  KC_X  KC_C      KC_V      KC_B    KC_N    KC_M      KC_COMMA KC_DOT KC_SLASH  KC_MINUS
+  KC_NO     KC_NO KC_NO KC_BSPACE LOWER_SPC KC_LCTL KC_RGUI RAISE_ENT KC_RALT  KC_NO  KC_NO     KC_NO
 ]
 
-kbd.add_layer :adjust, [
-# LEFT BOARD                                                  #RIGHT BOARD
-#|         |         |         |         |         |          |         |        |       |        |        |
-  :KC_F1,   :KC_F2,   :KC_F3,   :KC_F4,   :KC_F5,   :KC_F6,    :KC_F7,   :KC_F8,  :KC_F9, :KC_F10, :KC_F11, :KC_F12,
-  :RGB_TOG, :RGB_SPI, :RGB_HUI, :RGB_SAI, :RGB_VAI, :RGB_MOD,  :KC_NO,   :KC_NO,  :KC_NO, :KC_NO,  :KC_NO,  :BOOTSEL,
-  :RGB_TOG, :RGB_SPD, :RGB_HUD, :RGB_SAD, :RGB_VAD, :RGB_RMOD, :KC_NO,   :KC_NO,  :KC_NO, :KC_NO,  :KC_NO,  :KC_NO,
-  :KC_NO,   :KC_NO,   :KC_NO,   :DQ,      :ADJUST,  :KC_LCTL,  :SPC_CTL, :ADJUST, :DQ,    :KC_NO,  :KC_NO,  :KC_NO,
+kbd.add_layer :raise, %i[
+  KC_GRAVE KC_EXLM KC_AT   KC_HASH     KC_DLR  KC_PERC  KC_CIRC KC_AMPR   KC_ASTER KC_LPRN  KC_RPRN  KC_PIPE
+  KC_TAB   KC_LABK KC_LCBR KC_LBRACKET KC_LPRN KC_QUOTE KC_LEFT KC_DOWN   KC_UP    KC_RIGHT KC_UNDS  KC_PLUS
+  KC_LSFT  KC_RABK KC_RCBR KC_RBRACKET KC_RPRN KC_DQUO  KC_TILD KC_BSLASH KC_COMMA KC_DOT   KC_SLASH KC_EQUAL
+  KC_NO    KC_NO   KC_NO   KC_LANG1    ADJUST  KC_LCTL  KC_RGUI RAISE_ENT KC_RALT  KC_NO    KC_NO    KC_NO
+]
+
+kbd.add_layer :lower, %i[
+  KC_ESCAPE KC_1    KC_2    KC_3        KC_4      KC_5     KC_6    KC_7   KC_8     KC_9  KC_0     KC_PLUS
+  KC_TAB    KC_NO   KC_NO   KC_NO       KC_LPRN   KC_QUOTE KC_DOT  KC_4   KC_5     KC_6  KC_PLUS  KC_ASTER
+  KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_0    KC_1   KC_2     KC_3  KC_SLASH KC_COMMA
+  KC_NO     KC_NO   KC_NO   KC_BSPACE   LOWER_SPC KC_LCTL  KC_RGUI ADJUST KC_LANG2 KC_NO KC_NO    KC_NO
+]
+
+kbd.add_layer :adjust, %i[
+  KC_F1   KC_F2   KC_F3   KC_F4     KC_F5   KC_F6    KC_F7   KC_F8  KC_F9 KC_F10 KC_F11 KC_F12
+  RGB_TOG RGB_SPI RGB_HUI RGB_SAI   RGB_VAI RGB_MOD  KC_NO   KC_NO  KC_NO KC_NO  KC_NO  BOOTSEL
+  RGB_TOG RGB_SPD RGB_HUD RGB_SAD   RGB_VAD RGB_RMOD KC_NO   KC_NO  KC_NO KC_NO  KC_NO  KC_NO
+  KC_NO   KC_NO   KC_NO   KC_BSPACE ADJUST  KC_LCTL  SPC_CTL ADJUST KC_NO KC_NO  KC_NO  KC_NO
 ]
 
 kbd.define_composite_key :SPC_CTL, %i(KC_SPACE KC_RCTL)
@@ -93,17 +74,6 @@ rgb.speed = 28
 ].each do |p|
   rgb.add_pixel(p[0], p[1])
 end
+
 kbd.append rgb
-
-#
-# You only can get the right answer from `Keyboard#anchor?` inside the `Keyboard#on_start`
-# callback because anchor or not is undecided until the kbd starts.
-#
-# The song is split into 2 parts because memory is short if it's united.
-#
-kbd.on_start do
-end
-
-# When you tap :DQ key, the sounder will play the song's first half.
-# When you tap it again, you should hear the second part.
 kbd.start!
